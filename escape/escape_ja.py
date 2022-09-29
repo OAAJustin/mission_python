79c75da73b2096199abff099c1a30040a367923a
Justin Arroyo <justin.m.arroyo@protonmail.com>
2022-09-29 19:20:33 -0400

    Escape 1.3 - Draw Ship Behind Mars

--- escape/escape_ja.py
+++ escape/escape_ja.py
@@ -7,8 +7,9 @@ player_y = 350
 
 def draw():
     screen.blit(images.backdrop, (0, 0))
-    screen.blit(images.mars, (50, 50))
     screen.blit(images.ship, (130, 150))
+    screen.blit(images.mars, (50, 50))
+    
 
 
 
