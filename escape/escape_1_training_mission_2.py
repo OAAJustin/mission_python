744ee85dc0c317e7cd53ccf3fef6e6b5e34f86d9
Justin Arroyo <justin.m.arroyo@protonmail.com>
2022-09-29 21:22:46 -0400

    Escape Ch.1 Training Mission #2 - Hide Mars and Ship with One Line Ch…

--- escape/escape_1_training_mission_2.py
+++ escape/escape_1_training_mission_2.py
@@ -2,14 +2,13 @@ import pgzrun
 
 WIDTH = 800
 HEIGHT = 600
-player_x = 700
-player_y = 125
+player_x = 600
+player_y = 350
 
 
 def draw():
-    screen.blit(images.ship, (130,150))
     screen.blit(images.mars, (50, 50))
+    screen.blit(images.ship, (130,150))
     screen.blit(images.backdrop, (0, 0))
     
-
 pgzrun.go()
