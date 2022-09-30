f6a8c8225ac547f4c47882926864c8baebdfe9e0
Justin Arroyo <justin.m.arroyo@protonmail.com>
2022-09-29 21:15:52 -0400

    Add Astronaut - Generate Keyboard Loop to Move Character

--- escape/escape_1.4.py
+++ escape/escape_1.4.py
@@ -10,7 +10,7 @@ def draw():
     screen.blit(images.backdrop, (0, 0))
     screen.blit(images.mars, (50, 50))
     screen.blit(images.astronaut, (player_x, player_y))
-    screen.blit(images.ship, (650,50))
+    screen.blit(images.ship, (550,300))
     
 def game_loop():
     global player_x, player_y
@@ -19,9 +19,9 @@ def game_loop():
     elif keyboard.left:
         player_x -= 5
     elif keyboard.up:
-        player_y -= 10
+        player_y -= 5
     elif keyboard.down:
-        player_y += 10
+        player_y += 5
 
 clock.schedule_interval(game_loop, 0.03)
 
