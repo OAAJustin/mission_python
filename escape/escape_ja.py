dfe4bde0ef67795b6b4179085c0fcede4b164ca5
Justin Arroyo <justin.m.arroyo@protonmail.com>
2022-09-29 20:29:27 -0400

    List Work - Append, Remove, Index, Insert, Replace, and Delete

--- escape/escape_ja.py
+++ escape/escape_ja.py
@@ -5,6 +5,11 @@ HEIGHT = 600
 player_x = 700
 player_y = 125
 
+take_off_checklist = ["Put on suit", 
+                      "Seal hatch",
+                      "Check cabin pressure",
+                      "Fasten seatbelt"]
+
 def draw():
     screen.blit(images.backdrop, (0, 0))
     screen.blit(images.mars, (50, 50))
