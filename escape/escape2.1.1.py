20ac0eb2f5f3e6fe108fe8be5fe71be5c2b06496
Justin Arroyo <justin.m.arroyo@protonmail.com>
2022-09-29 21:43:27 -0400

    Add Take Off Checklist, List and View

---
+++ escape/escape2.1.1.py
@@ -0,0 +1,33 @@
+import pgzrun
+
+WIDTH = 800
+HEIGHT = 600
+player_x = 650
+player_y = 100
+
+take_off_checklist = ["Put on suit",
+                      "seal hatch", 
+                      "Check Cabin Pressure",
+                      "Fasten seatbelt"]
+print(take_off_checklist)
+
+def draw():
+    screen.blit(images.backdrop, (0, 0))
+    screen.blit(images.mars, (50, 50))
+    screen.blit(images.astronaut, (player_x, player_y))
+    screen.blit(images.ship, (600, 50))
+    
+def game_loop():
+    global player_x, player_y
+    if keyboard.right:
+        player_x += 5
+    elif keyboard.left:
+        player_x -= 5
+    elif keyboard.up:
+        player_y -= 10
+    elif keyboard.down:
+        player_y += 10
+
+clock.schedule_interval(game_loop, 0.03)
+    
+pgzrun.go()
